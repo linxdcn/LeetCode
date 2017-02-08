dd9360cfba09df0d6c4906934efbd7d6a2a9e871
Xiaodong Lin <linxdcn@gmail.com>
2017-02-08 14:20:50 +0800

    add NO.442

---
+++ 442-FindDuplicatesInArray/FindDuplicatesInArray.java
@@ -0,0 +1,13 @@
+public class Solution {
+    public List<Integer> findDuplicates(int[] nums) {
+        List<Integer> result = new ArrayList<Integer>();
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[Math.abs(nums[i]) - 1] < 0) {
+                result.add(Math.abs(nums[i]));
+            } else {
+                nums[Math.abs(nums[i]) - 1] = -nums[Math.abs(nums[i]) - 1];
+            }
+        }
+        return result;
+    }
+}
